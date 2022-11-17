89a164b315bc751b72ae5bfaad48e5e3089a3c43
adeshagarwal28 <59112633+adeshagarwal28@users.noreply.github.com>
2022-11-16 21:52:03 -0500

    Logout functionality implemented
    Added logout functionality

--- app/src/main/java/com/example/grocerydelivery/MainActivity.kt
+++ app/src/main/java/com/example/grocerydelivery/MainActivity.kt
@@ -19,38 +19,20 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
-//import com.google.firebase.firestore.ktx.firestore
-//import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val TAG = "GroceryDeliveryAndroidDebug"
     private var mAuth: FirebaseAuth? = null
-    //private val db = Firebase.firestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        /*
-        val firstFragment=HomeFragment()
-        val secondFragment=SearchFragment()
-        val thirdFragment=CartFragment()
-        val fourthFragment=ProfileFragment()
-
-         */
-        //mAuth = FirebaseAuth.getInstance()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        /*binding.buttonLogin.setOnClickListener{
-            Log.d(ContentValues.TAG,"Login Button Clicked")
-            val action= HomeFragmentDirections.actionNavigationHomeToLoginActivity()
-            findNavController().navigate(action) }
-        binding.buttonSignUp.setOnClickListener{
-            Log.d(ContentValues.TAG,"Sign up Button Clicked")
-            val action1= HomeFragmentDirections.actionNavigationHomeToRegisterActivity()
-            findNavController().navigate(action1)}*/
         binding.buttonLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
@@ -61,19 +43,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        /*val navView: BottomNavigationView? = binding.bottomNavigationView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_cart, R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        navView?.setupWithNavController(navController)*/
     }
 
 
--- app/src/main/java/com/example/grocerydelivery/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/grocerydelivery/ui/profile/ProfileFragment.kt
@@ -5,10 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.grocerydelivery.R
 import com.example.grocerydelivery.databinding.FragmentProfileBinding
+import com.example.grocerydelivery.ui.home.HomeFragmentDirections
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -16,10 +21,9 @@ import com.google.firebase.ktx.Firebase
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private val TAG="GroceryAndroidDebug"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,19 +55,27 @@ class ProfileFragment : Fragment() {
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getToken() instead.
             val uid = user.uid
+
+            auth=Firebase.auth
+
+            binding.buttonLogout.setOnClickListener{
+                Firebase.auth.signOut()
+                val action= ProfileFragmentDirections.actionNavigationProfileToMainActivity()
+                findNavController().navigate(action)
+            }
+
         }
 
-
-
-        //val textView: TextView = binding.textNotifications
-        //notificationsViewModel.text.observe(viewLifecycleOwner) {
-          //  textView.text = it
-        //}
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun performLogout()
+    {
+
     }
 }
